ff502f8506a24dd341ccbadfaac25d1b8abb7492
Eric <eric.george@idean.com>
2020-07-10 13:39:51 +0000

    configs to run on any repl.it hostname

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  config.hosts << /[a-z0-9]+\.repl\.co/
+  config.hosts << /[-a-zA-Z0-9@:%._\+~#=]+\.repl\.co/
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
